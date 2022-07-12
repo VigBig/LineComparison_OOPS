bd67e9f979df15a6dfeb3c9269aee891ae25c5cf
VighneshBilgi <vhbilgi@gmail.com>
2022-07-12 19:07:40 +0530

    Added: UC3_Comparison_Of_Two_Lines

--- src/com/bridgelabz/linecomparison/LineComparison.java
+++ src/com/bridgelabz/linecomparison/LineComparison.java
@@ -40,6 +40,7 @@ public class LineComparison {
         System.out.println("Length of Line 1 is: " + length1);
         System.out.println("Length of Line 2 is: " + length2);
 
+        System.out.println("Comparing lines with .equals() method");
         if( (Double.toString(length1) ).equals( Double.toString(length2) ) == true){
             System.out.println("The Lines are equal");
         }
@@ -47,5 +48,20 @@ public class LineComparison {
             System.out.println("The Lines are NOT equal");
         }
 
+        System.out.println("Comparing lines with .compareTo() method");
+        int compare_to =  (Double.toString(length1) ).compareTo( Double.toString(length2) );
+        // "compare_to" variable holds the integer value outputted by .compareTo() method.
+        // If compare_to is -ve , then Line 1 is smaller ,
+        // else if compare_to is +ve , then Line 1 is larger
+        // else Line 1 is equal to Line 2
+
+        if(compare_to <0){
+            System.out.println("Line 1 is lesser than Line 2");
+        } else if (compare_to >0) {
+            System.out.println("Line 1 is greater than Line 2");
+        }
+        else {
+            System.out.println("Line 1 is equal to Line 2");
+        }
     }
 }
